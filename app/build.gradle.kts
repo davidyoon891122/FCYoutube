5683d11f6613917f25a432ba8951e325f91ec122
David Yoon <davidyoon@hanwha.com>
2024-12-23 10:30:58 +0900

    Add a Thumnail Image recyclerView
    * Added a recyclerView to display thumnail image and description text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.gson)
+    implementation(libs.glide)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/fcyoutube/MainActivity.kt
+++ app/src/main/java/com/example/fcyoutube/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fcyoutube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var videoAdapter: VideoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        videoAdapter = VideoAdapter(context = this)
+
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
         }
+
+        val videoList = readData("videos.json", VideoList::class.java) ?: VideoList(emptyList())
+        videoAdapter.submitList(videoList.videos)
     }
 }
